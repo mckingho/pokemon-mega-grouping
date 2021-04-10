f0ba87b66a5c48186a2287a9b961f1f2ea5aff6c
Michael Cheung <mc.kingho@gmail.com>
2021-04-10 18:35:51 +0800

    Format code

--- src/min_mega_pokemon_type_coverage.rs
+++ src/min_mega_pokemon_type_coverage.rs
@@ -1,9 +1,9 @@
 use std::collections::{HashMap, HashSet};
 
-use crate::pokemon::{MegaPokemons};
+use crate::pokemon::MegaPokemons;
 use crate::r#type::TypeBitFlag;
 
-use gamma::graph::{DefaultGraph};
+use gamma::graph::DefaultGraph;
 use gamma::matching::{maximum_matching, Pairing};
 
 pub fn find_min_mega_for_type_coverage() {
@@ -18,7 +18,7 @@ pub fn find_min_mega_for_type_coverage_with_primals() {
 
 fn find_min_for_type_coverage(megas: MegaPokemons) {
     let type_mons_map = build_type_mons_map(&megas);
-    
+
     let mut types_graph = DefaultGraph::new();
     let types: Vec<TypeBitFlag> = type_mons_map.keys().cloned().collect();
     for t in types.into_iter() {
@@ -36,8 +36,18 @@ fn find_min_for_type_coverage(megas: MegaPokemons) {
     for (pair_type_a, pair_type_b) in types_pairing.edges() {
         let type_a = TypeBitFlag::num_to_enum(pair_type_a).unwrap();
         let type_b = TypeBitFlag::num_to_enum(pair_type_b).unwrap();
-        let type_a_megas: HashSet<&str> = type_mons_map.get(&type_a).unwrap().iter().cloned().collect();
-        let type_b_megas: HashSet<&str> = type_mons_map.get(&type_b).unwrap().iter().cloned().collect();
+        let type_a_megas: HashSet<&str> = type_mons_map
+            .get(&type_a)
+            .unwrap()
+            .iter()
+            .cloned()
+            .collect();
+        let type_b_megas: HashSet<&str> = type_mons_map
+            .get(&type_b)
+            .unwrap()
+            .iter()
+            .cloned()
+            .collect();
         let intersection_megas = type_a_megas.intersection(&type_b_megas);
         println!("{:?},{:?}: {:?}", type_a, type_b, intersection_megas);
     }
@@ -46,7 +56,7 @@ fn find_min_for_type_coverage(megas: MegaPokemons) {
 fn build_type_mons_map(megas: &MegaPokemons) -> HashMap<TypeBitFlag, Vec<&str>> {
     let mut type_megas: HashMap<TypeBitFlag, Vec<&str>> = HashMap::new();
     for mon in megas.0.iter() {
-        let mut types = vec!(mon.type_1);
+        let mut types = vec![mon.type_1];
         if let Some(type_2) = mon.type_2 {
             types.push(type_2);
         }
@@ -54,9 +64,9 @@ fn build_type_mons_map(megas: &MegaPokemons) -> HashMap<TypeBitFlag, Vec<&str>> 
             match type_megas.get_mut(&t) {
                 Some(mons) => {
                     mons.push(&mon.name);
-                },
+                }
                 None => {
-                    type_megas.insert(t, vec!(&mon.name));
+                    type_megas.insert(t, vec![&mon.name]);
                 }
             }
         }
@@ -68,9 +78,15 @@ pub fn print_per_type_ascending() {
     let megas = MegaPokemons::new();
 
     let type_mons_map = build_type_mons_map(&megas);
-    
+
     let mut types: Vec<TypeBitFlag> = type_mons_map.keys().cloned().collect();
-    types.sort_by(|a, b| type_mons_map.get(a).unwrap().len().cmp(&type_mons_map.get(b).unwrap().len()));
+    types.sort_by(|a, b| {
+        type_mons_map
+            .get(a)
+            .unwrap()
+            .len()
+            .cmp(&type_mons_map.get(b).unwrap().len())
+    });
 
     for t in types.iter() {
         let mons = type_mons_map.get(t).unwrap();
