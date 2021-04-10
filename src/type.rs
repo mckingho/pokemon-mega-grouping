d2fb50bf0a0285864abdfa0c812efb0777bea0f3
Michael Cheung <mc.kingho@gmail.com>
2021-04-10 18:34:55 +0800

    Use num_enum for number enum conversion

--- src/type.rs
+++ src/type.rs
@@ -1,6 +1,9 @@
+use num_enum::TryFromPrimitive;
 use serde::{Deserialize, Serialize};
+use std::convert::TryFrom;
 
-#[derive(Clone, Copy, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
+#[derive(Clone, Copy, Debug, Deserialize, Eq, Hash, PartialEq, Serialize, TryFromPrimitive)]
+#[repr(usize)]
 pub enum TypeBitFlag {
     Normal = 1,
     Fighting = 2,
@@ -24,26 +27,9 @@ pub enum TypeBitFlag {
 
 impl TypeBitFlag {
     pub fn num_to_enum(v: usize) -> Option<TypeBitFlag> {
-        match v {
-            1 => Some(TypeBitFlag::Normal),
-            2 => Some(TypeBitFlag::Fighting),
-            4 => Some(TypeBitFlag::Flying),
-            8 => Some(TypeBitFlag::Poison),
-            16 => Some(TypeBitFlag::Ground),
-            32 => Some(TypeBitFlag::Rock),
-            64 => Some(TypeBitFlag::Bug),
-            128 => Some(TypeBitFlag::Ghost),
-            256 => Some(TypeBitFlag::Steel),
-            512 => Some(TypeBitFlag::Fire),
-            1024 => Some(TypeBitFlag::Water),
-            2048 => Some(TypeBitFlag::Grass),
-            4096 => Some(TypeBitFlag::Electric),
-            8192 => Some(TypeBitFlag::Psychic),
-            16384 => Some(TypeBitFlag::Ice),
-            32768 => Some(TypeBitFlag::Dragon),
-            65536 => Some(TypeBitFlag::Dark),
-            131072 => Some(TypeBitFlag::Fairy),
-            _ => None,
+        match Self::try_from(v) {
+            Ok(t) => Some(t),
+            Err(_) => None,
         }
     }
 }
